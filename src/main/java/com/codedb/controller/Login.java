76601ca8d06db0c05cff3717bcf2aeebffa90a7e
cz2542079957 <2542079957@qq.com>
2022-10-06 14:07:48 +0800

    f

--- src/main/java/com/codedb/controller/Login.java
+++ src/main/java/com/codedb/controller/Login.java
@@ -1,5 +1,22 @@
 package com.codedb.controller;
 
-public class Login
-{
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Login {
+  @FXML private TextField dbName;
+
+  @FXML private TextField userName;
+
+  @FXML private TextField passWord;
+
+  @FXML private Button connection;
+
+  public void connect(MouseEvent e) {
+    String dbNameText = dbName.getText();
+    String userNameText = userName.getText();
+    String passWordText = passWord.getText();
+  }
 }
